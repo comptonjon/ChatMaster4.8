724997b2ebf72a5173c723e2528d1613a9d3c00c
Jon Compton <yar12345@hotmail.com>
2018-06-26 15:36:24 -0700

    Basic UI Setup.  Create ChooseLoginVC, LoginVC, SignUpVC, YourChatsVC, AllUsersVC, and MessagesVC.

---
+++ ChatMaster4.8/AllUsersVC.swift
@@ -0,0 +1,35 @@
+//
+//  AllUsersVC.swift
+//  ChatMaster4.8
+//
+//  Created by Jonathan Compton on 6/26/18.
+//  Copyright © 2018 Jonathan Compton. All rights reserved.
+//
+
+import UIKit
+
+class AllUsersVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
